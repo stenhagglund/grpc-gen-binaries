d120940e2245fa1b6b94f1911c1d76b6b0f91f43
Sten Hägglund <sten.hagglund@gmail.com>
2024-11-03 12:00:01 +0200

    chore: remove manual build scripts

--- build_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p dist/
-
-docker build --platform=linux/amd64 -t grpc-gen-binaries .
-docker container rm grpc-gen-binaries > /dev/null
-docker run --name grpc-gen-binaries grpc-gen-binaries /bin/true
-docker cp grpc-gen-binaries:/usr/local/bin/protoc-gen-python-grpc-linux-amd64 ./dist/protoc-gen-python-grpc-linux-amd64
--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd dist
-rm -f sha256.txt
-sha256sum $(ls . | grep -v sha256.txt) >> sha256.txt
-
-cd ..
-gh release create v1.66.1 ./dist/* --notes "Binaries built from https://github.com/grpc/grpc/releases/tag/v1.66.1"
